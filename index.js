53e5be84618f5a2935ff7ae4d5f0170d7ee79ef5
walkingalchemy <walkingalchemy@gmail.com>
2018-01-13 20:45:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function printBadges (names) {
+  for (i=0;i<names.length;i++) {
+    console.log(`Welcome ${names[i]}! You are employee #${i+1}.`)
+  }
+  return names
+}
+
+function tailsNeverFails() {
+  let count = 0
+  while (Math.random() >= 0.5) {
+    count++
+  }
+  return `You got ${count} tails in a row!`
+}
